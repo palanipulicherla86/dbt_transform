bcc59171683bf2cd2b6f19252e57b6fbdf5cb5f2
palanipulicherla86@gmail.com <palanipulicherla86@gmail.com>
2023-06-14 17:07:34 +0000

    l

---
+++ models/staging/tpch/stg_customer.sql
@@ -0,0 +1,25 @@
+
+with source as (
+
+    select * from {{ source('tpch', 'customer') }}
+
+),
+
+renamed as ( 
+
+    select
+    
+    C_CUSTKEY as custkey ,
+    C_NAME as name ,
+    C_ADDRESS as address ,
+    C_NATIONKEY as nationkey ,
+    C_PHONE as phone ,
+    C_ACCTBAL as acctbal ,
+    C_MKTSEGMENT as mktsegment ,
+    C_COMMENT as comment 
+
+    from source
+
+)
+
+select * from renamed
